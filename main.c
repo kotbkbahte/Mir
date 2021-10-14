d449d1308ca6c08b2e59c9f675930ab73d805790
kotbkbahte <kot-b-kbahte@yandex.ru>
2021-10-14 22:29:47 +0300

    Math commit

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int sum(int, int);
+
 int main()
 {
+    printf("Test!\n");
     printf("Hello world!\n");
+    printf("%d\n", sum(10, 15));
     return 0;
 }
